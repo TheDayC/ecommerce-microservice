208c0a2136bc209f366304a007430a35da781512
TheDayC <daveconnolly90@gmail.com>
2020-11-14 08:28:43 +0000

    Added 3 endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,41 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import memCache from 'memory-cache';
 
-import * as products from './db/products.json';
+import * as db from './db/index.json';
 
-import {cacheData, loadProducts} from './middleware/cache';
+import {cacheData} from './middleware/cache';
 import {index} from './routes';
-import {addProduct} from './routes/products';
+import { addProduct, removeProduct, clearCart} from './routes/products';
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 const cache = new memCache.Cache();
+const {products, cart} = db; // Destructure items from temp DB.
 
-// Before we even serve a route, load our products into a cache so they can be manipulated.
-cacheData('products', products.items, 100);
+// Before we even serve a route, load our products and cart into a cache so they can be manipulated.
+// Ideally this should be middleware and abstracted.
+cacheData('products', products, 100, cache);
+cacheData('cart', cart, 100, cache);
 
 // Fetch root automatically to display something on load
 app.get('/', index);
 
-// Add item
-app.post('/products/add', loadProducts('products', cache), addProduct);
+// Add item to cart
+app.post('/cart/add/:id', addProduct(cache));
+
+// Remove item from cart
+app.post('/cart/remove/:id', removeProduct(cache));
+
+// Remove all items from cart
+app.post('/cart/clear', clearCart(cache));
 
 // Set Express to list to our port provided in the dotenv config file.
 app.listen(port, () => {
     console.log(`Express started at http://localhost:${port}`)
 });
+
+app.on('uncaughtException', (err) => {
+    console.log("Uncaught exception: " + err.stack);
+})
--- src/middleware/cache.ts
+++ src/middleware/cache.ts
@@ -1,19 +1,5 @@
-// import { Request } from 'express';
-import { NextFunction, Response } from 'express';
-import memoryCache, { CacheClass } from 'memory-cache';
-import { IProduct } from '../types/db';
-import { CustomRequest } from '../types/express';
+import { Cache, IProduct } from '../types/db';
 
-export function cacheData(id: string, data: IProduct[], duration: number): void {
-    memoryCache.put(id, data, duration);
-}
-
-export function loadProducts(id: string, cache: CacheClass<unknown, unknown>): any {
-    return (req: CustomRequest, res: Response, next: NextFunction) => {
-        const products = cache.get(id) as IProduct[]; // Ideally this would be parsed properly from unknown to IProduct[] using a typeguard
-        
-        req.products = products ? products : undefined;
-
-        next();
-    }
+export function cacheData(id: string, data: IProduct[], duration: number, cache: Cache): void {
+    cache.put(id, data, duration);
 }
